5799d82b4225b3e77545b22eeea468f06ff297bc
Brad Heller <brad@aboutus.org>
2011-09-25 15:39:38 -0700

    Fix installer, few other bits.

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 apt-get install ruby build-essential libopenssl-ruby ruby1.8-dev
 curl -o /opt/rubygems.tgz http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz
 
-cd /opt/rubygems.tgz
+cd /opt
 tar -xvf rubygems.tgz
 ruby ./rubygems/setup.rb
+cd -
 
 # Install chef
 gem install chef
 
 # Now install the requisite cookbooks!
+chef-solo -c ./solo.rb ./app_server.json
