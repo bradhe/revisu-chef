ff33085dc468e80a93c644451f432532d02ce69d
Brad Heller <brad@aboutus.org>
2011-09-25 16:48:12 -0700

    Okay, fine, don't use that one, use mine instead.

--- cookbooks/revisu/recipes/default.rb
+++ cookbooks/revisu/recipes/default.rb
@@ -1,22 +1,25 @@
+
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+node.set_unless['mysql']['server_root_password'] = secure_password
+
 %w{/www/revisu/current /www/revisu/release /www/revisu/shared}.each do |d|
   directory d do
     mode 0755
-    owner 'root'
-    group 'root'
+    owner 'ubuntu'
+    group 'ubuntu'
     recursive true
     action :create
   end
 end
 
-template "nginx.conf" do
-  path "#{node[:nginx][:dir]}/nginx.conf"
-  source "nginx.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-end
-
 gem_package "passenger" do
   action :install
   ignore_failure false
+end
+
+template '/www/revisu/shared/database.yml' do
+  source 'database.yml.erb'
+  mode 0755
+  owner 'ubuntu'
+  group 'ubuntu'
 end
